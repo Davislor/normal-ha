bc6c51537251e67238a3d04ab77eaeaca5ce4a91
Loren Davis <Lorehead@zoho.com>
2020-06-11 19:58:39 -0700

    Refactored into components.

--- app/Main.hs
+++ app/Main.hs
@@ -9,15 +9,26 @@
 
 module Main (main) where
 
-import Data.ByteString.Lazy as BL (interact)
-import Data.Text.Lazy as TL ( fromChunks, toChunks )
-import Data.Text.Lazy.Encoding ( encodeUtf8, decodeUtf8 )
+import Data.ByteString.Builder ( Builder, toLazyByteString )
+import Data.ByteString.Lazy as BL ( ByteString, interact )
+import Data.Text as T (Text)
+import Data.Text.Lazy as TL (toChunks)
+import Data.Text.Lazy.Encoding as LE (decodeUtf8)
+import Data.Text.Encoding as E (encodeUtf8Builder)
 import Data.Text.ICU ( NormalizationMode (NFC), normalize )
 
 main :: IO ()
 main = BL.interact (
-         encodeUtf8 .
-         TL.fromChunks .
-         map (normalize NFC) .
-         TL.toChunks . -- FIXME: Does this have correct grapheme boundaries?
-         decodeUtf8 )
+         toLazyByteString .
+         mconcat .
+         Prelude.map normalizeChunk .
+         decode )
+
+decode :: BL.ByteString -> [T.Text]
+decode = TL.toChunks . -- FIXME: Break on grapheme boundaries instead?
+         LE.decodeUtf8
+         
+normalizeChunk :: T.Text -> Builder
+normalizeChunk = encodeUtf8Builder .
+                 normalize NFC
+
