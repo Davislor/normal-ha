7ec80b1bb1a802cb0708976b789d7aa239872775
Loren Davis <Lorehead@zoho.com>
2020-06-12 01:39:28 -0700

    Added license notices and comments to test programs.

--- test/normalTest.hs
+++ test/normalTest.hs
@@ -1,3 +1,12 @@
+{----------------------------------------------------------------------------
+ - Copyright © 2020 Loren B. Davis.  All rights reserved.                   -
+ -                                                                          -
+ - This program is hereby released under the three-clause BSD license (see  -
+ - ../LICENSE).                                                             -
+ - normalTest: Tests that the data on standard input is UTF-8 in NFC canon- -
+ - ical form.                                                               -
+ ----------------------------------------------------------------------------}
+
 module Main (main) where
 
 import Data.Text as T (pack)
--- test/strictTest.hs
+++ test/strictTest.hs
@@ -1,3 +1,12 @@
+{----------------------------------------------------------------------------
+ - Copyright © 2020 Loren B. Davis.  All rights reserved.                   -
+ -                                                                          -
+ - This program is hereby released under the three-clause BSD license (see  -
+ - ../LICENSE).                                                             -
+ - strictTest.hs: A simple wrapper for Data.Text.ICU.normalize.  The prog-  -
+ - ram runs only in batch mode, and all data must fit into its memory.      -
+ ----------------------------------------------------------------------------}
+
 module Main (main) where
 
 import Data.Text.IO as TIO (interact)
