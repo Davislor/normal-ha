80d86c0921a3a5c072ce0a0bd296445aa1ea3ae0
Loren Davis <Lorehead@zoho.com>
2020-06-11 22:27:11 -0700

    Processes data in lazy chunks, interactively.

--- app/Main.hs
+++ app/Main.hs
@@ -10,12 +10,13 @@
 module Main (main) where
 
 import Data.ByteString.Builder ( Builder, toLazyByteString )
+import Data.ByteString.Builder.Extra (byteStringInsert)
 import Data.ByteString.Lazy as BL ( ByteString, interact )
-import Data.Text as T (Text)
-import Data.Text.Lazy as TL (toChunks)
+import Data.Text as T -- (Text, empty)
+import Data.Text.Lazy as TL -- (Text, empty)
 import Data.Text.Lazy.Encoding as LE (decodeUtf8)
-import Data.Text.Encoding as E (encodeUtf8Builder)
-import Data.Text.ICU ( NormalizationMode (NFC), normalize )
+import Data.Text.Encoding as E (encodeUtf8)
+import Data.Text.ICU -- ( NormalizationMode (NFC), normalize )
 
 main :: IO ()
 main = BL.interact (
@@ -24,10 +25,32 @@ main = BL.interact (
          decode )
 
 decode :: BL.ByteString -> [T.Text]
-decode = TL.toChunks . -- FIXME: Break on grapheme boundaries instead?
+{- Breaks the lazy ByteString into a list of strict ByteStrings, each of which
+ - ends on a grapheme boundary, and whose concatenation is the same as the
+ - original input.
+ -
+ - This uses the pure Breaker API, finding the last grapheme break in each
+ - chunk and then copying the leftovers from the previous chunk, plus every-
+ - thing up to the last grapheme boundary in this chunk, to a new chunk. This
+ - makes more deep copies than necessary.
+ -}
+decode = go T.empty .
+         toChunks .
          LE.decodeUtf8
-         
+  where go :: T.Text -> [T.Text] -> [T.Text]
+        go left [] | left == T.empty = []
+        go left []                   = [left]
+        go left (h:t)                  =
+          ((TL.toStrict . TL.fromChunks) [left, middle, right]):
+          (go residue t)
+          where graphemeBreaker = breakCharacter Current
+                lastGrapheme = Prelude.head (breaksRight graphemeBreaker h)
+                middle = brkPrefix lastGrapheme
+                right = brkBreak lastGrapheme
+                residue = brkSuffix lastGrapheme
+
 normalizeChunk :: T.Text -> Builder
-normalizeChunk = encodeUtf8Builder .
+normalizeChunk = byteStringInsert .
+                 E.encodeUtf8 .
                  normalize NFC
 
